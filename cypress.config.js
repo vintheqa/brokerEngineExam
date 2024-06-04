aeab8f63cef579c58ecffdba83f4677b0f3df81b
vintheqa <marvinrilleracolorado@gmail.com>
2024-06-04 21:45:11 +0800

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs');
+const path = require('path');
+const XLSX = require('xlsx');
+const { removeDirectory } = require('cypress-delete-downloads-folder');
 
 module.exports = defineConfig({
   viewportWidth: 1920,
@@ -11,12 +15,37 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {
+        createFolder(folderPath) {
+          if (!fs.existsSync(folderPath)) {
+              fs.mkdirSync(folderPath, { recursive: true });
+          }
+          return null;
+      },
+        findLatestXlsx(directory) {
+          const files = fs.readdirSync(directory);
+          const xlsxFiles = files.filter(file => file.endsWith('.xlsx'));
+
+          if (xlsxFiles.length === 0) {
+              throw new Error('No .xlsx files found in the specified directory');
+          }
+
+          // Find the latest .xlsx file by modified time
+          const latestFile = xlsxFiles
+              .map(file => ({
+                  file,
+                  time: fs.statSync(path.join(directory, file)).mtime.getTime()
+              }))
+              .sort((a, b) => b.time - a.time)[0].file;
+
+          return path.join(directory, latestFile);
+      },
         parseXlsx({ filePath }) {
             const workbook = XLSX.readFile(filePath);
-            const sheetNames = workbook.SheetNames;
-            const sheet = workbook.Sheets[sheetNames[0]];
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
             return XLSX.utils.sheet_to_json(sheet, { header: 1 });
-        }
+        },  
+        removeDirectory 
     });
     },
     supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
